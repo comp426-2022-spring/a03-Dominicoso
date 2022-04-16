ef41f38fbf3bd91034742339a6b91b1c86f07c59
Dominicoso <dominicoso@gmail.com>
2022-04-16 13:05:55 -0400

    added stuff again

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%',port))
 });
 
-app.use(function(req, res){
-    res.status(404).send('404 NOT FOUND')
-});
-
 app.get('/app/', (req, res) => {
     res.status(200).end('OK')
     res.type('text/plain')
@@ -28,3 +24,7 @@ app.get('/app/', (req, res) => {
 app.get('app/echo/:number', (req, res) => {
     res.status(200).json({ 'message': req.params.number })
 })
+
+app.use(function(req, res){
+    res.status(404).send('404 NOT FOUND')
+});
