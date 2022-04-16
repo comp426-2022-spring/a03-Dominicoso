d9702d52a87d4131b9b28f16c812d123c93b89a7
Dominicoso <dominicoso@gmail.com>
2022-04-16 13:01:32 -0400

    asdf

--- server.js
+++ server.js
@@ -16,14 +16,14 @@ app.use(function(req, res){
 });
 
 app.get('/app/', (req, res) => {
-    res.status(200).end('OK')
-    res.type('text/plain')
-    // // Respond with status 200
-    //     res.statusCode = 200;
-    // // Respond with status message "OK"
-    //     res.statusMessage = 'OK';
-    //     res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
-    //     res.end(res.statusCode+ ' ' +res.statusMessage)
+    // res.status(200).end('OK')
+    // res.type('text/plain')
+    // Respond with status 200
+        res.statusCode = 200;
+    // Respond with status message "OK"
+        res.statusMessage = 'OK';
+        res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
+        res.end(res.statusCode+ ' ' +res.statusMessage)
     });
 
 app.get('app/echo/:number', (req, res) => {
