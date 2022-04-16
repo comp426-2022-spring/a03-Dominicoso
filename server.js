cbaa91b592b847a442e84b47485fe96ef943d645
Dominicoso <dominicoso@gmail.com>
2022-04-16 13:32:16 -0400

    almost there

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { get } = require('http')
 const app = express()
 
 const args = require('minimist')(process.argv.slice(2))
@@ -10,6 +11,44 @@ const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%',port))
 });
 
+function coinFlip() {
+    const result = (0 == Math.round(Math.random()) ? "heads" : "tails")
+    return result
+}
+
+function coinFlips(flips) {
+    let result = []
+    for (let i = 0; i < flips; i++) {
+      result[i] = coinFlip()
+    }
+    return result
+}
+
+function countFlips(array) {
+    const result = {
+      heads: 0,
+      tails: 0,
+    }
+    let res = [0, 0]
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] == "heads") {
+        result.heads++
+      } else {
+        result.tails++
+      }
+    }
+    return result
+}
+
+function flipACoin(call) {
+    const result = {
+      call: call
+    }
+    result.flip = coinFlip()
+    result.result = call == result.flip ? "win" : "lose"
+    return result;
+  }
+
 app.get('/app/', (req, res) => {
     // res.status(200).end('OK')
     // Respond with status 200
@@ -20,8 +59,21 @@ app.get('/app/', (req, res) => {
         res.end(res.statusCode+ ' ' +res.statusMessage)
     });
 
-app.get('app/echo/:number', (req, res) => {
-    res.status(200).json({ 'message': req.params.number })
+app.get('/app/flip/', (req, res) => {
+    res.status(200).json({ 'flip' : coinFlip() })
+});
+
+app.get('/app/flip/call/heads', (req, res) => {
+    res.status(200).send(flipACoin('heads'))
+})
+
+app.get('/app/flip/call/tails', (req, res) => {
+    res.status(200).send(flipACoin('tails'))
+})
+
+app.get('/app/flips/:number', (req, res) => {
+    const arr = coinFlips(req.params.number)
+    res.status(200).json({ 'raw' : arr, 'summary' : countFlips(arr) })
 })
 
 app.use(function(req, res){
